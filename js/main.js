931ed8479d2f72d6b4798a053ab90042979f4855
Александра <aleksa_kolesnik@mail.ru>
2020-02-11 23:55:04 +0500

    Личный проект: больше деталей (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,74 +1,52 @@
 'use strict';
-var roomType = '';
+
+var OFFSET_X = 25;
+var OFFSET_Y = 70;
+var COUNT_PINS = 8;
+
+var arrayData = [];
 var roomTypes = ['palace', 'flat', 'house', 'bungalo'];
-var roomTypesToGenerate = function (roomTypesPar) {
-  roomType = roomTypesPar[Math.floor(Math.random() * roomTypesPar.length)];
-  return roomType;
-};// генератор типа размещения
-var titleType = '';
 var titleTypes = ['Рэддисон Измайлово', 'Отель Мария', 'Гостиница Брусника', 'BAKUNIN Apartment', 'Гостиница Охотник', 'Гостиница Москвич'];
-var titleTypesToGenerate = function (titleTypesPar) {
-  titleType = titleTypesPar[Math.floor(Math.random() * titleTypesPar.length)];
-  return titleType;
-};
-var description = '';
 var descriptions = ['Превосходно', 'Выше ожидаемого', 'Великолепно', 'Хорошо', 'Не плохо'];
-var descriptionsToGenerate = function (descriptionsPar) {
-  roomType = descriptionsPar[Math.floor(Math.random() * descriptions.length)];
-  return description;
-};
-
-roomTypesToGenerate(roomTypes);// генератор заголовка
-
-var features = [];
 var featuresVariant = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var featuresGenerator = function (featuresVariantPar) {
-  var f = 0;
-  for (f < Math.ceil(Math.random() * featuresVariantPar.length); f++;) {
-    features[f] = featuresVariantPar[f];
-  }
-  return features;
-};
-featuresGenerator(featuresVariant);// генератор удобств
 var times = ['12:00', '13:00', '14:00'];
-var timesToGenerate = function (timesPar) {
-  var time = timesPar[Math.floor(Math.random() * times.length)];
-  return time;
-};
-timesToGenerate(times);// генератор времен заезда и выезда
-
 var photosAll = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var photosToGenerate = function (photosAllPar) {
-  var photos = [];
-  for (var i = 0; i < photosAllPar.length; i++) {
-    photos[i] = photosAllPar[i];
+
+var arrayRandomToCreate = function (arrayParametrs) {
+  var p = 0;
+  for (p < Math.ceil(Math.random() * arrayParametrs.length); p++;) {
+    arrayData[p] = arrayParametrs[p];
   }
-  return photos;
+  return arrayData;
 };
-photosToGenerate(photosAll);// генератор картинок
 
+var stringRandomToCreate = function (arraySrings) {
+  var string = arraySrings[Math.floor(Math.random() * arraySrings.length)];
+  return string;
+};
 
-var mapWidth = 1140;// размер блока в котором перетаскивается метка 1200-20-40
+var mapPins = document.querySelector('.map__pins');
+var mapPinsWidth = mapPins.offsetWidth;
 
-var ArrayMokis = [];// массив куда соберем сгенерировнные обьекты
+var arrayMocks = [];// массив куда соберем сгенерировнные обьекты
 
 var mokiToGenerate = function () {
-  for (var g = 1; g < 9; g++) {
-    ArrayMokis[g - 1] = {'author': {'avatar': 'img/avatars/user' + '0' + g + '.png'},
+  for (var g = 0; g < COUNT_PINS; g++) {
+    arrayMocks[g] = {'author': {'avatar': 'img/avatars/user' + '0' + (g + 1) + '.png'},
       'offer':
-                          {'title': titleTypesToGenerate(titleTypes),
-                            'address': Math.floor(Math.random() * document.documentElement.clientWidth) + ',' + (130 + Math.floor(Math.random() * (630 - 130))),
+                          {'title': stringRandomToCreate(titleTypes),
+                            'address': OFFSET_X + Math.floor(Math.random() * (mapPinsWidth - OFFSET_X * 2)) + ',' + (130 + Math.floor(Math.random() * (630 - 130))),
                             'price': 1000 + Math.floor(Math.random() * 3000),
-                            'type': roomTypesToGenerate(roomTypes),
+                            'type': stringRandomToCreate(roomTypes),
                             'rooms': Math.ceil(Math.random() * 3),
                             'guests': Math.ceil(Math.random() * 5),
-                            'checkin': timesToGenerate(times),
-                            'checkout': timesToGenerate(times),
-                            'features': featuresGenerator(featuresVariant),
-                            'description': descriptionsToGenerate(descriptions),
-                            'photos': photosToGenerate(photosAll)},
+                            'checkin': stringRandomToCreate(times),
+                            'checkout': stringRandomToCreate(times),
+                            'features': arrayRandomToCreate(featuresVariant),
+                            'description': stringRandomToCreate(descriptions),
+                            'photos': arrayRandomToCreate(photosAll)},
       'location': {
-        'x': Math.floor(Math.random() * mapWidth),
+        'x': OFFSET_X + Math.floor(Math.random() * (mapPinsWidth - OFFSET_X * 2)),
         'y': 130 + Math.floor(Math.random() * (500))
       }};
   }
@@ -79,23 +57,19 @@ mokiToGenerate();// генератор обьектов
 var mapBlock = document.querySelector('.map');
 mapBlock.classList.remove('map--faded');
 
-var X = 20; // смещение по х от края метки до указателя
-var Y = 84; // смещение по y
-
 var pinTemplate = document.querySelector('#pin').content;
-var mapPins = document.querySelector('.map__pins');
 
 var pinsDraw = function () {
-  for (var j = 0; j < 8; j++) {
+  for (var j = 0; j < COUNT_PINS; j++) {
     var clonePinTemplate = pinTemplate.cloneNode(true);
     var pinButton = clonePinTemplate.querySelector('button');
-    var pinButtonLocationX = ArrayMokis[j].location.x + X;
-    var pinButtonLocationY = ArrayMokis[j].location.y - Y;
+    var pinButtonLocationX = arrayMocks[j].location.x - OFFSET_X;
+    var pinButtonLocationY = arrayMocks[j].location.y - OFFSET_Y;
     pinButton.style.left = pinButtonLocationX + 'px';
     pinButton.style.top = pinButtonLocationY + 'px';
     var pinButtonImage = pinButton.querySelector('img');
-    pinButtonImage.src = ArrayMokis[j].author.avatar;
-    pinButtonImage.alt = ArrayMokis[j].offer.title;
+    pinButtonImage.src = arrayMocks[j].author.avatar;
+    pinButtonImage.alt = arrayMocks[j].offer.title;
     mapPins.appendChild(clonePinTemplate);
   }
 };
